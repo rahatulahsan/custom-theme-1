69e1b2edcc69eb14fbbc22a80365149b206d14bb
Rahatul Ahsan <rahatulahsan@gmail.com>
2023-05-30 23:28:58 +0600

    Added Professor Post Type and Cleaned Code
    Added Professor Post Type and Cleaned Code

---
+++ wp-content/themes/fictional-univesity/single-professor.php
@@ -0,0 +1,54 @@
+<?php 
+
+get_header();
+
+while(have_posts(  )) {
+    the_post(  ); 
+    
+    // function written in functions.php
+    pageBanner();
+    
+    ?>
+
+
+
+    <div class="container container--narrow page-section">
+
+        <div class="generic-content">
+            <div class="row group">
+                <div class="one-third">
+                    <?php the_post_thumbnail('professorPotrait'); ?>
+                </div>
+                <div class="two-thirds">
+                    <?php the_content(); ?>
+                </div>
+
+            </div>
+        </div>
+
+    
+       <?php 
+
+        $relatedPrograms = get_field('related_programs');
+
+        if($relatedPrograms){
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
+            echo '<ul class="link-list min-list">';
+            foreach($relatedPrograms as $program){?>
+    
+                <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+                
+            <?php }
+            echo '</ul>';
+        }
+        
+       ?>
+
+    </div>
+    
+<?php }
+
+get_footer();
+
+?>
